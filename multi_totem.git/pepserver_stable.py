d84ceaea8d23449054c2448e0fca4a3953c7c9e5
dpapadi <dpapadimass@gmail.com>
2016-07-27 17:07:09 +0300

    added address_mapping

--- multi_totem.git/pepserver_stable.py
+++ multi_totem.git/pepserver_stable.py
@@ -47,7 +47,7 @@ def address_mapping(ten_id, ten_ip): #ten_id --> tenant id (example 1)
     for phIp, virtIp in tmp.iteritems():    #phIp   --> Ip visible to controller
         if virtIp == ten_ip:                #virtIp --> Ip visible to OVX
             print virtIp + "-->" + phIp
-            raw_in  put() #to test mapping
+            raw_input() #to test mapping
             return phIp
     else:
         print "There is no address mapping for %s in Tenant Network: %s" % ten_ip % ten_id
