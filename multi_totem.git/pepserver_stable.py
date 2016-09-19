e13fd6b50b0f89ba0c5a467f71f7c2cbfdcfedab
dpapadi <dpapadimass@gmail.com>
2016-09-19 13:47:35 +0300

    test for port mapping

--- multi_totem.git/pepserver_stable.py
+++ multi_totem.git/pepserver_stable.py
@@ -418,8 +418,8 @@ def collect_sflow(flow):
                 print "Correct port mapping!"
             else:
                 print "Wrong port mapping!"
-                print "Wrong port:  " + port
-                print "Correct port:" + mac_table[dpid][sflow['srcMAC']]
+                print "Wrong port:  %s" % port
+                print "Correct port:%s" % mac_table[dpid][sflow['srcMAC']]
         else:
             if sflow['srcMAC'][:8] == 'a4:23:05':
                 tid = sflow['srcMAC'][10:11]
