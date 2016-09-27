bc87ac13ace8c28c44f6f80e96749daf977c684d
dpapadi <dpapadimass@gmail.com>
2016-09-27 14:16:31 +0300

    added dpid_mapping

--- multi_totem.git/pepserver_stable.py
+++ multi_totem.git/pepserver_stable.py
@@ -62,7 +62,8 @@ def dpid_mapping(dpid, mac):
         tmp = ast.literal_eval(os.popen(commands[1]).read()) #find the dpid throught the VirtualSwitchMapping of ovxctl
         new_dpid = tmp[mod_dpid]['switches'][1]
         return new_dpid
-    except:
+    except Exception, e:
+        print e
         print "Error in dpid_mapping function"
         return dpid
 
