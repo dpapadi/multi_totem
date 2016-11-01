735595caa1767c8616ee9249abd57f37aa2d0c76
dpapadi <dpapadimass@gmail.com>
2016-11-01 14:07:27 +0200

    modify sflow.py for queue

--- multi_totem.git/sflow.py
+++ multi_totem.git/sflow.py
@@ -135,7 +135,7 @@ def sflowParser():
             # The above is used not to double-count the flows, since counter is initialized in one
             # The above has to be modified since in our case we drop traffic
 
-            producer.send_messages(sys.argv[3], b)
+            #producer.send_messages(sys.argv[3], b)
             #try:
                 #server.collect_sflow(b)
             #except  Exception,e:
