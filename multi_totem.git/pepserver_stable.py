9be33df4b52734933d1b8caf8cf3fbca911f5e24
dpapadi <dpapadimass@gmail.com>
2017-01-07 16:19:55 +0200

    client.py first

--- multi_totem.git/pepserver_stable.py
+++ multi_totem.git/pepserver_stable.py
@@ -517,9 +517,9 @@ if __name__ == "__main__":
             cl_req = client_consumer.get_message()
             print cl_req
             raw_input()
-            if cl_req is not None:
-                server.handle_request()
-                cl_req = False
+            #if cl_req is not None:
+             #   server.handle_request()
+              #  cl_req = False
             msg = main_consumer.get_message()
             #for message in consumer:
             get_input_from_queue(msg.message.value)
