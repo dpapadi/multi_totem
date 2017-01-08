f9aab902e42ff1de67659a631dba6ca149f7add0
dpapadi <dpapadimass@gmail.com>
2017-01-08 23:10:39 +0200

    client.py third

--- multi_totem.git/pepserver_stable.py
+++ multi_totem.git/pepserver_stable.py
@@ -521,7 +521,7 @@ if __name__ == "__main__":
             #raw_input()
             print cl_req
             if bool(cl_req):
-                server.handle_request()
+                #server.handle_request()
                 cl_req={}
             #msg = main_consumer.next()
             #for message in consumer:
