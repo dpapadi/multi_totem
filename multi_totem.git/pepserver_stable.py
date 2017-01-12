50f201054a739ffb398bff6fad8bf0c03a96a832
dpapadi <dpapadimass@gmail.com>
2017-01-12 13:09:39 +0200

    client.py me timeout

--- multi_totem.git/pepserver_stable.py
+++ multi_totem.git/pepserver_stable.py
@@ -531,9 +531,9 @@ if __name__ == "__main__":
         while True:
         #try:
             cl_req = client_consumer.poll()
-            print str(cl_req.values()).split()[6]
             raw_input()
             if bool(cl_req):
+                print str(cl_req.values()).split()[6]
                 print "handle request"
                 handle_request()
             msg = main_consumer.poll()
