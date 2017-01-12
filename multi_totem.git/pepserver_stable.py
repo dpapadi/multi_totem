ffe6e98f9ba0c3af08d8097a898864a45c8e866d
dpapadi <dpapadimass@gmail.com>
2017-01-12 14:02:45 +0200

    get_input_from_queue ksana

--- multi_totem.git/pepserver_stable.py
+++ multi_totem.git/pepserver_stable.py
@@ -537,6 +537,7 @@ if __name__ == "__main__":
             msg = main_consumer.poll()
             if bool(msg):
                 i = len(str(msg.values()).split()[6]) - 1
+                print str(msg.values()).split()[6][6:i]
                 get_input_from_queue(str(msg.values()).split()[6][6:i])
             #raw_input()
             #if bool(msg):
