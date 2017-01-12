c386e4d6e01c9cda33abd7892329c33a1ffb570e
dpapadi <dpapadimass@gmail.com>
2017-01-12 14:08:48 +0200

    get_input_from_queue ksana

--- multi_totem.git/sflow.py
+++ multi_totem.git/sflow.py
@@ -128,6 +128,8 @@ def sflowParser():
 
             args = ("collect_sflow", flow)
             b = pickle.dumps(args)
+            print b
+            raw_input
             #b = pickle.dumps(flow)
             #if 'outputPort' in flow:
              #   server.collect_sflow(b)
