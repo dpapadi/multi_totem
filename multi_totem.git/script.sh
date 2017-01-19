8a161143ef9a55f9fcdcfa56701f78ea0337d1cb
dpapadi <dpapadimass@gmail.com>
2017-01-19 17:13:10 +0200

    scriptaki

--- multi_totem.git/script.sh
+++ multi_totem.git/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd ~/kafka/kafka_2.11-0.10.1.0
-sh bin/zookeeper-server-start.sh config/zookeeper.properties
-sh bin/kafka-server-start.sh config/server.properties
+sh bin/zookeeper-server-start.sh config/zookeeper.properties &
+sh bin/kafka-server-start.sh config/server.properties &
