989f1e7000b6890e360577bf4096cc0724c2f852
dpapadi <dpapadimass@gmail.com>
2017-06-21 14:35:46 +0300

    testing topo

--- multi_totem.git/testing_topo0.py
+++ multi_totem.git/testing_topo0.py
@@ -16,50 +16,50 @@ CORES = {
 
 FANOUT = 5
 
-class TestingTop(Topo):
+class TestingTopo(Topo):
 
     def __init__(self, enable_all = True):
         "Create my testing topology."
 
-    # Add default members to class.
-    super(TestingTop, self).__init__()
+        # Add default members to class.
+         super(TestingTopo, self).__init__()
 
-    # Add core switches
-    self.cores = {}
-    for switch in CORES:
-        self.cores[switch] = self.addSwitch(switch, dpid=(CORES[switch]['dpid'] % '0'))
+         # Add core switches
+         self.cores = {}
+         for switch in CORES:
+            self.cores[switch] = self.addSwitch(switch, dpid=(CORES[switch]['dpid'] % '0'))
 
-    #used to customize host ips
-    mycount = 1
+         #used to customize host ips
+         mycount = 1
 
-    # Add hosts and connect them to their core switch
-    for switch in CORES:
-        for count in xrange(1, FANOUT + 1):
-            # Add hosts
-            host = '%s_%s' % (switch, count)
-            ip = '%s.0.0.%s' % (count, mycount)
-            mac = CORES[switch]['dpid'][4:] % mycount
-            h = self.addHost(host, ip=ip, mac=mac)
-            # Connect hosts to core switches
-            self.addLink(h, self.cores[switch])
-        mycount+=1
+         # Add hosts and connect them to their core switch
+         for switch in CORES:
+             for count in xrange(1, FANOUT + 1):
+                 # Add hosts
+                 host = '%s_%s' % (switch, count)
+                 ip = '%s.0.0.%s' % (count, mycount)
+                 mac = CORES[switch]['dpid'][4:] % mycount
+                 h = self.addHost(host, ip=ip, mac=mac)
+                 # Connect hosts to core switches
+                 self.addLink(h, self.cores[switch])
+             mycount+=1
 
-        # Connect core switches
-    self.addLink(self.cores['S1'], self.cores['S2'])
-    self.addLink(self.cores['S1'], self.cores['S3'])
-    self.addLink(self.cores['S2'], self.cores['S3'])
-    self.addLink(self.cores['S2'], self.cores['S4'])
-    self.addLink(self.cores['S3'], self.cores['S4'])
-    self.addLink(self.cores['S4'], self.cores['S5'])
+             # Connect core switches
+         self.addLink(self.cores['S1'], self.cores['S2'])
+         self.addLink(self.cores['S1'], self.cores['S3'])
+         self.addLink(self.cores['S2'], self.cores['S3'])
+         self.addLink(self.cores['S2'], self.cores['S4'])
+         self.addLink(self.cores['S3'], self.cores['S4'])
+         self.addLink(self.cores['S4'], self.cores['S5'])
 
-    if __name__ == '__main__':
-        topo = TestingTop()
-        ip = '127.0.0.1'
-        port = 6633
-        c = RemoteController('c', ip=ip, port=port)
-        net = Mininet(topo=topo, autoSetMacs=True, xterms=False, controller=None)
-        net.addController(c)
-        net.start()
-        print "Hosts configured with IPs, switches pointing to OpenVirteX at %s:%s" % (ip, port)
-        CLI(net)
-        net.stop()
+if __name__ == '__main__':
+    topo = TestingTopo()
+    ip = '127.0.0.1'
+    port = 6633
+    c = RemoteController('c', ip=ip, port=port)
+    net = Mininet(topo=topo, autoSetMacs=True, xterms=False, controller=None)
+    net.addController(c)
+    net.start()
+    print "Hosts configured with IPs, switches pointing to OpenVirteX at %s:%s" % (ip, port)
+    CLI(net)
+    net.stop()
