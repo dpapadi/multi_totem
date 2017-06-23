8a09c4dc49ed4d572e1467ca1d13fa29ee0ecd9a
dpapadi <dpapadimass@gmail.com>
2017-06-23 11:47:59 +0300

    testing topo

--- multi_totem.git/testing_topo0.py
+++ multi_totem.git/testing_topo0.py
@@ -9,11 +9,11 @@ from mininet.cli import CLI
 from mininet.node import RemoteController
 
 CORES = {
-  'S1': {'dpid': '000000000000010%s'},
-  'S2': {'dpid': '000000000000020%s'},
-  'S3': {'dpid': '000000000000030%s'},
-  'S4': {'dpid': '000000000000040%s'},
-  'S5': {'dpid': '000000000000050%s'}
+  'S1': {'dpid': '000000000000010%s', 'id': '1'},
+  'S2': {'dpid': '000000000000020%s', 'id': '2'},
+  'S3': {'dpid': '000000000000030%s', 'id': '3'},
+  'S4': {'dpid': '000000000000040%s', 'id': '4'},
+  'S5': {'dpid': '000000000000050%s', 'id': '5'}
   }
 
 FANOUT = 5
@@ -31,20 +31,18 @@ class TestingTopo(Topo):
         for switch in CORES:
             self.cores[switch] = self.addSwitch(switch, dpid=(CORES[switch]['dpid'] % '0'))
 
-        #used to customize host ips
-        mycount = 1
 
         # Add hosts and connect them to their core switch
         for switch in CORES:
             for count in xrange(1, FANOUT + 1):
                 # Add hosts
                 host = '%s_%s' % (switch, count)
-                ip = '10.0.%s.%s' % (count, mycount)
+                ip = '10.0.%s.%s' % (count, CORES[switch]['id'])
                 mac = CORES[switch]['dpid'][4:] % count
                 h = self.addHost(host, ip=ip, mac=mac)
                 # Connect hosts to core switches
                 self.addLink(h, self.cores[switch])
-            mycount+=1
+
 
              # Connect core switches
         self.addLink(self.cores['S1'], self.cores['S2'])
