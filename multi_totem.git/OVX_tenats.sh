a9212110e12e07775349735039b1fbddea8bbb05
dpapadi <dpapadimass@gmail.com>
2017-06-23 15:20:25 +0300

    ovx tenants script

--- multi_totem.git/OVX_tenats.sh
+++ multi_totem.git/OVX_tenats.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]
+then
+    echo "$0 needs more arguments. (normal, 2bsw, bsw, vlink, all) controller_port1 etc"
+fi
 
+case "1" in
 
-echo "$1"
+    normal) echo "normal: $@"
+            exit
+            ;;
+    2bsw)   echo "2bsw: $@"
+            exit
+            ;;
+    bsw)    echo "bsw: $@"
+            exit
+            ;;
+    vlink)  echo "vlink: $@"
+            exit
+            ;;
+    all)    echo "all: $@"
+            exit
+            ;;
+    *)      echo "wrong first attribute"
+            exit
+            ;;
+esac
