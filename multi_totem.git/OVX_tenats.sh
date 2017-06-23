2029566885300ef73801cfb5fc00d0c076e3d8a2
dpapadi <dpapadimass@gmail.com>
2017-06-23 15:24:11 +0300

    ovx tenants script

--- multi_totem.git/OVX_tenats.sh
+++ multi_totem.git/OVX_tenats.sh
@@ -5,21 +5,46 @@ then
     echo "$0 needs more arguments. (normal, 2bsw, bsw, vlink, all) controller_port1 etc"
 fi
 
-case "1" in
+case "$1" in
 
-    normal) echo "normal: $@"
+    normal) if [ $# -ne 2 ]
+            then
+                echo "wrong number of inputs!!!!"
+                exit
+            fi
+            echo "normal: $@"
             exit
             ;;
-    2bsw)   echo "2bsw: $@"
+    2bsw)   if [ $# -ne 2 ]
+            then
+                echo "wrong number of inputs!!!!"
+                exit
+            fi
+            echo "2bsw: $@"
             exit
             ;;
-    bsw)    echo "bsw: $@"
+    bsw)    if [ $# -ne 2 ]
+            then
+                echo "wrong number of inputs!!!!"
+                exit
+            fi
+            echo "bsw: $@"
             exit
             ;;
-    vlink)  echo "vlink: $@"
+    vlink)  if [ $# -ne 2 ]
+            then
+                echo "wrong number of inputs!!!!"
+                exit
+            fi
+            echo "vlink: $@"
             exit
             ;;
-    all)    echo "all: $@"
+    all)    if [ $# -ne 2 ]
+            then
+                echo "wrong number of inputs!!!!"
+                exit
+            fi
+            echo "all: $@"
             exit
             ;;
     *)      echo "wrong first attribute"
