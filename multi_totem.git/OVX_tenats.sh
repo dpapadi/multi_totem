2b3eb495e79def96eaed9494b1908f11618c57c1
dpapadi <dpapadimass@gmail.com>
2017-06-23 16:03:00 +0300

    ovx tenants script

--- multi_totem.git/OVX_tenats.sh
+++ multi_totem.git/OVX_tenats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $1 == "normal" ] || [ $1 == "2bsw" ] || [ $1 == "bsw" ] || [ $1 == "vlink" ]
+if [[ $1 == "normal" ] || [ $1 == "2bsw" ] || [ $1 == "bsw" ] || [ $1 == "vlink" ]] && [ $# -ne 2 ]
 then
     echo "Provide 2 arguments (normal, 2bsw, bsw, vlink) port"
     echo "You provided $# arguments"
