e255e87be7731972aead41387d20761c5ca4ec5d
DiM <dpapadimass@gmail.com>
2017-07-09 12:59:16 +0300

    pepserver_stable move_to_expired

--- multi_totem.git/pepserver_stable.py
+++ multi_totem.git/pepserver_stable.py
@@ -213,7 +213,7 @@ def move_to_expired(args, scnd=False):
     e = construct_hashed_key(match)
 
     while True:
-    #try:
+    try:
     # if there is such a flow
         if e in active[tid][dpid]:
             # remove the flow
@@ -254,9 +254,9 @@ def move_to_expired(args, scnd=False):
                 print "FAILED AGAIN!!!!"
                 return
 
-   # except:
-    #    print "error in wide try of move to expired!"
-     #   return
+    except:
+        print "error in wide try of move to expired!"
+        return
 
 
 def collect_sflow(flow):
