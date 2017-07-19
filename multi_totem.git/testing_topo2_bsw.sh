54f740645abd2114d3aba85f069a8a2a7b0f3b68
DiM <dpapadimass@gmail.com>
2017-07-19 16:49:49 +0300

    testing_topo2

--- multi_totem.git/testing_topo2_bsw.sh
+++ multi_totem.git/testing_topo2_bsw.sh
@@ -31,14 +31,14 @@ python ovxctl.py -n connectLink 1 00:a4:23:05:00:00:00:01 2 00:a4:23:05:00:00:00
 python ovxctl.py -n connectLink 1 00:a4:23:05:00:00:00:01 3 00:a4:23:05:00:00:00:03 3 spf 1
 python ovxctl.py -n connectLink 1 00:a4:23:05:00:00:00:01 4 00:a4:23:05:00:00:00:05 3 spf 1
 
-python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:02 2 00:00:00:00:02:21
-python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:02 1 00:00:00:00:02:22
-python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:03 1 00:00:00:00:03:31
-python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:03 2 00:00:00:00:03:32
-python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:04 2 00:00:00:00:04:41
-python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:04 1 00:00:00:00:04:42
-python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:05 1 00:00:00:00:05:51
-python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:05 2 00:00:00:00:05:52
+python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:02 2 00:00:00:00:05:51
+python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:02 1 00:00:00:00:05:52
+python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:03 1 00:00:00:00:06:61
+python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:03 2 00:00:00:00:06:62
+python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:04 2 00:00:00:00:07:71
+python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:04 1 00:00:00:00:07:72
+python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:05 1 00:00:00:00:08:81
+python ovxctl.py -n connectHost 1 00:a4:23:05:00:00:00:05 2 00:00:00:00:08:82
 
 python ovxctl.py -n startNetwork 1
 
